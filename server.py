46d4794ac5baca3698c79589e4bcd05d0ce2e6ce
viejopeter <pquinchanegua@gmail.com>
2025-07-09 22:58:17 -0400

    Passed pylint static analysis with 10.00/10 score

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" This App is for sentiment analysis """
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,19 +6,18 @@ app = Flask("Sentiment Detector")
 
 @app.route("/")
 def index():
+    """ Return the default index page """
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def sent_emotion_detector():
-
+    """ Analyze the text and return response """
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
-    
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    else:
-        return f"for the given statement, the system response is {response}"
-       
-if __name__ == "__main__":
-   app.run(host="0.0.0.0",port=5000)
 
+    return f"for the given statement, the system response is {response}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
