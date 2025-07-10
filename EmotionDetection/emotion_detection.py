1bb15fd5e845539d9ed0c27625f546db67700a76
viejopeter <pquinchanegua@gmail.com>
2025-07-09 20:29:07 -0400

    Output formatted,packaged function,unit test executed

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,7 +1,12 @@
-import requests
+import requests,json
+
 def emotion_detector(text_to_analyze):
     url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": { "text": text_to_analyze }}
     response = requests.post(url, headers=headers, json=myobj )
-    return  response.text
+    jsonFormated = json.loads(response.text)
+    emotions = jsonFormated['emotionPredictions'][0]['emotion']
+    nameDominant = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = nameDominant
+    return  emotions
